0ffac55991279f1621aaab204a499b9eee9568d5
sarath1412k <kumar17cs035@gmail.com>
2024-04-05 08:07:31 +0530

    initial coomit with finshed code of task

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
+import CircularProgress from '@mui/material/CircularProgress';
+import Card from '@mui/material/Card';
 import './App.css';
 
-function App() {
+// import Dexie from 'dexie';
+// import { useLiveQuery } from 'dexie-react-hooks';
+import axios from 'axios';
+
+// export const db = new Dexie("randomData");
+// db.version(2).stores({
+//   randomData: 'id,text,completed',
+// });
+
+
+const App:React.FC =() => {
+  
+  const [loading,setLoading] = useState(false)
+  // const randomData = useLiveQuery(() => db.table("randomData").toArray(), []);
+  const [data,setData] = useState([])
+
+
+  useEffect(() => {    
+    if(localStorage.getItem("randomData")){
+      const result:any = localStorage.getItem("randomData")
+      setData(JSON.parse(result))
+    }
+    else{
+      setLoading(true)
+      axios.get(`https://randomuser.me/api/?results=50`)
+      .then(async(res:any) =>{
+        setTimeout(async() => {
+          //await db.table("randomData").add({ randomData:res.data.results});
+          setData(res.data.results)
+          localStorage.setItem('randomData',JSON.stringify(res.data.results))
+          setLoading(false)
+        },1000)
+      })
+      .catch((err:any) => {
+        console.error(err);setLoading(false)
+      })
+    }
+
+  },[])
+
+  const handleRefresh = async () => {
+    setLoading(true) 
+    axios.get(`https://randomuser.me/api/?results=50`)
+    .then(async(res:any) =>{
+      setTimeout(async() => {
+        //await db.table("randomData").add({ randomData:res.data.results});
+        setData(res.data.results)
+        localStorage.setItem('randomData',JSON.stringify(res.data.results))
+        setLoading(false)
+
+      },1000)
+    })
+    .catch((err:any) => {
+    console.error(err);setLoading(false)
+    })
+}
+
+  const onDelete = (index:any) => {
+     let filterData = data.filter((e,i) => 
+      i !== index
+      )
+      setData(filterData)
+      localStorage.setItem('randomData',JSON.stringify(filterData))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button className='refresh-btn' onClick={handleRefresh}>Refresh</button>
+    <span className='item-indicator'>Total number of items: {data.length}</span>
+    {loading && <div className='mat-spin'><CircularProgress  /></div>}
+    <div className='cards-grid'>
+    {data.map((obj,index) => {return(
+        <div className='card-mat' key={index}>
+        <Card className='card-container' sx={{ minWidth: 155,minHeight:200 }}>
+        <img className="profile-pic" src={obj["picture"]["large"]} alt="Profile-img" />
+        <div className="profile-name">{`${obj["name"]["first"]} ${obj["name"]["last"]}`}</div>
+        <button onClick={() => onDelete(index)} className="delete-btn">Delete</button>
+        </Card>
+        </div>
+    )})
+   }
+    </div>
+
     </div>
   );
 }
